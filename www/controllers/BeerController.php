bdcad674026125ddc19faca2f73487f80f25600a
Mirziano <Spagnolo_mg@outlook.com>
2023-05-08 10:41:54 +0200

    Modification des commentaires

--- www/controllers/BeerController.php
+++ www/controllers/BeerController.php
@@ -4,38 +4,30 @@
     class BeerController extends BaseController{
 
             /**
-     * ---- TODO : Commenter cette méthode ----
-     * Création d une me
+     * 
      */
     public function getList() {
         try {
-          // ---- TODO : Commenter ce bout de code ----
           $beerModel = new Beer();
   
-          // ---- TODO : Commenter ce bout de code ----
           $limit = 10;
           $urlParams = $this->getQueryStringParams();
           if (isset($urlParams['limit']) && is_numeric($urlParams['limit'])) {
             $limit = $urlParams['limit'];
           }
   
-          // ---- TODO : Commenter ce bout de code ----
           $offset = 0;
           $urlParams = $this->getQueryStringParams();
           if (isset($urlParams['page']) && is_numeric($urlParams['page']) && $urlParams['page'] > 0) {
             $offset = ($urlParams['page'] - 1) * $limit;
           }
   
-          // ---- TODO : Commenter ce bout de code ----
           $beers = $beerModel->getAllBeers($offset, $limit);
   
-          // ---- TODO : Commenter ce bout de code ----
           $responseData = json_encode($beers);
   
-          // ---- TODO : Commenter ce bout de code ----
           $this->sendOutput($responseData);
         } catch (Error $e) {
-          // ---- TODO : Commenter ce bout de code ----
           $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
           $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
           $this->sendOutput($strErrorDesc, ['Content-Type: application/json', $strErrorHeader]);
@@ -45,25 +37,19 @@
       //Création de la deuxieme methode pour selectionner une biere selon son id
       public function get() {
         try {
-          // ---- TODO : Commenter ce bout de code ----
           $beerModel = new Beer();
   
-          // ---- TODO : Commenter ce bout de code ----
           $urlParams = $this->getQueryStringParams();
           if (!isset($urlParams['id']) || !is_numeric($urlParams['id'])) {
             throw new Exception("L'identifiant est incorrect ou n'a pas été spécifié");
           }
   
-          // ---- TODO : Commenter ce bout de code ----
           $beer = $beerModel->getSingleBeer($urlParams['id']);
   
-          // ---- TODO : Commenter ce bout de code ----
           $responseData = json_encode($beer);
   
-          // ---- TODO : Commenter ce bout de code ----
           $this->sendOutput($responseData);
         } catch (Error $e) {
-          // ---- TODO : Commenter ce bout de code ----
           $strErrorDesc = $e->getMessage().'Something went wrong! Please contact support.';
           $strErrorHeader = 'HTTP/1.1 500 Internal Server Error';
           $this->sendOutput($strErrorDesc, ['Content-Type: application/json', $strErrorHeader]);
@@ -72,16 +58,13 @@
 
       public function store() {
         try {
-          // ---- TODO : Commenter ce bout de code ----
           $beerModel = new Beer();
   
-          // ---- TODO : Commenter ce bout de code ----
           $body = $this->getBody();
           if (!$body) {
             throw new Exception("Aucune donnée n'a été transmise dans le formulaire");
           }
   
-          //gestion des erreurs si un champ n'est pas spécifié
           if (!isset($body['id'])) {
             throw new Exception("Aucun id n'a été spécifié");
           }
@@ -107,7 +90,6 @@
             throw new Exception("Aucune date n'a été spécifié");
           }
   
-          // nous allons creer un tableau vide qui va venir contenir les choses à mettre à jour et données id name etc
           $keys = array_keys($body);
           $valuesToInsert = [];
           foreach($keys as $key) {
@@ -116,10 +98,8 @@
             }
           }
   
-          //commande qui va insérer nos info
           $beer = $beerModel->insertBeer($valuesToInsert);
   
-          //nous allons encoder les infos en format json et renvoyé le statut true avec son code de succes 
           $responseData = json_encode(array(
             "statuts" => true,
             "success" => 200
